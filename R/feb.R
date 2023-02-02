751efbc9ad6211a56f0c24aa0aa1eeb6e056de89
xtiandres <xtiancuencap@gmail.com>
2023-02-01 21:49:27 -0500

    ini ok

---
+++ R/feb.R
@@ -0,0 +1,22 @@
+# ELECCIONES 2023
+
+# LIBRERIAS
+library(dplyr)
+library(tidyr)
+library(plyr)
+library(readr)
+library(ggplot2)
+library(vcd)
+library(ggridges)
+library(data.table)
+library(lubridate)
+library(tidyverse)
+library(RColorBrewer)
+library(patchwork)
+library(ggpubr)
+library(EpiEstim)
+
+# DATASETS
+dat <- read_csv('/home/xut/Documents/udaviz/R/studio/ecfeb23/data/data.csv')
+
+
